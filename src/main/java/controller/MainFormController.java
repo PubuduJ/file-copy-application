e9462a0003059f8dbf497f7018c13cae7ff07e7f
Pubudu Janith <pubudujanith123@gmail.com>
2022-11-18 18:58:17 +0530

    Close #2

--- src/main/java/controller/MainFormController.java
+++ src/main/java/controller/MainFormController.java
@@ -1,5 +1,39 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+
 public class MainFormController {
 
+    public AnchorPane pneContainer;
+    public JFXButton btnSelectFile;
+    public JFXButton btnSelectDirectory;
+    public Label lblSelectedFile;
+    public Label lblBrowsedDirectory;
+    public JFXButton btnBrowseDirectory;
+    public Label lblProgress;
+    public Label lblSize;
+    public Rectangle pgbBar;
+    public Rectangle pgbContainer;
+    public Button btnCopy;
+
+    public void btnSelectFile_OnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnSelectDirectory_OnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnBrowseDirectory_OnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnCopy_OnAction(ActionEvent actionEvent) {
+
+    }
 }
