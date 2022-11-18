b09c38cde9857751db32f2d796a61871e0bdd314
Pubudu Janith <pubudujanith123@gmail.com>
2022-11-18 19:09:13 +0530

    Complete file-selection method logic

--- src/main/java/controller/MainFormController.java
+++ src/main/java/controller/MainFormController.java
@@ -1,11 +1,19 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.util.Duration;
+
+import java.io.File;
+import java.text.NumberFormat;
+import java.util.List;
 
 public class MainFormController {
 
@@ -20,9 +28,39 @@ public class MainFormController {
     public Rectangle pgbBar;
     public Rectangle pgbContainer;
     public Button btnCopy;
+    private List<File> srcFiles;
+    private File srcDirectory;
+    private File destDir;
+
+    public void initialize() {
+        FadeTransition fd = new FadeTransition(Duration.millis(1500),pneContainer);
+        fd.setFromValue(0);
+        fd.setToValue(1);
+        fd.playFromStart();
+        btnCopy.setDisable(true);
+    }
 
     public void btnSelectFile_OnAction(ActionEvent actionEvent) {
-
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.setTitle("Select files to copy");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("All Files (*.*)", "*.*"));
+        srcFiles = fileChooser.showOpenMultipleDialog(btnSelectFile.getScene().getWindow());
+        if (srcFiles != null) {
+            srcDirectory = null;
+            double size = 0;
+            if (srcFiles.size() > 1) {
+                for (File file : srcFiles) {
+                    size += file.length();
+                }
+                lblSelectedFile.setText("Multiple files have been selected, size: " + formatNumber(size/1024.0) + " Kb");
+            }
+            else if (srcFiles.size() == 1) {
+                lblSelectedFile.setText("File name: " + srcFiles.get(0).getName() + ", size: " + formatNumber(srcFiles.get(0).length()/1024.0) + " Kb");
+            }
+        }
+        else lblSelectedFile.setText("No file/directory selected");
+        btnCopy.setDisable(srcFiles == null || destDir == null);
     }
 
     public void btnSelectDirectory_OnAction(ActionEvent actionEvent) {
@@ -35,5 +73,13 @@ public class MainFormController {
 
     public void btnCopy_OnAction(ActionEvent actionEvent) {
 
+    }
+
+    private String formatNumber(double input) {
+        NumberFormat number = NumberFormat.getNumberInstance();
+        number.setGroupingUsed(true);
+        number.setMinimumFractionDigits(2);
+        number.setMaximumFractionDigits(2);
+        return number.format(input);
     }
 }
